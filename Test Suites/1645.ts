8685548db004e3c1f5f18eb31a2b41c1252b8de4
JLEMAITRECHRONO <JLEMAITRE@CHRONODRIVE.COM>
2022-12-12 18:00:55 +0100

    05

--- Test Suites/1645.ts
+++ Test Suites/1645.ts
@@ -5,10 +5,23 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>ce337af9-4b84-4e32-a9c0-8c77a1e78dd7</testSuiteGuid>
+   <testCaseLink>
+      <guid>e60631f0-06b8-4edf-884a-cb289165f74c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/chemin critique 01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3d4fc67d-a512-47f3-b4d3-e5594b7cecec</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
